c6b61cbc876a9c70546d2b6be0e112b226dbc495
Abdelrahman-El-Kassas <elkassas380@gmail.com>
2026-02-23 00:41:06 +0200

    with env file you can upload from supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:alnabil/features/videos_screen/presentation/screens/videos_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // أضف هذا السطر
 
-void main() {
+Future<void> main() async {
+  // السطر ده مهم جداً قبل أي await في الـ main
+  WidgetsFlutterBinding.ensureInitialized(); 
+
+  // تحميل ملف المتغيرات
+  await dotenv.load(fileName: ".env");
+
+  // تهيئة Supabase باستخدام المتغيرات المخفية
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const MyApp());
 }
 
